ea7d750aea5dd584eb0b5b127d1f281a6fd4b13e
YuDarvish <drollrisoka@gmail.com>
2019-10-13 23:01:51 -0300

    create a checkpoint system

---
+++ Assets/UTech/MG-Platformer/BasicAssets/Scripts/Mechanics/CheckPointZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using Platformer.Gameplay;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// CheckPointZone components mark a collider which will schedule a
+    /// PlayerEnteredDeathZone event when the player enters the trigger.
+    /// </summary>
+    /// 
+    public class CheckPointZone : MonoBehaviour
+    {
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            if(!model.spawnPoint.Contains(transform))
+                model.spawnPoint.Add(transform);
+        }
+    }
+}
